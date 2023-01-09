866e0cab40d9c61a3ed5d2f6736cf632153d2b08
SharkGate <122305510+website-protection-sharkgate@users.noreply.github.com>
2023-01-09 18:39:34 +0000

    initial version

---
+++ README.md
@@ -0,0 +1,37 @@
+# SharkGate
+
+Protect you website from attacks with SharkGate's Cloudflare App. SharkGate is a website security platform that protects
+your website from attacks and malicious traffic. It's easy to install and works with any website.
+
+The app also allows you to tell your visitors that your website is protected and accelerated by SharkGate by adding a badge to your site.
+Not only do you let your visitors know you’re fast and safe, but you’re scoring some serious karma by spreading the word about SharkGate!
+
+
+
+## Setup
+
+Fork and clone the repo or <a href="https://github.com/website-protection-sharkgate/cloudflare-app/releases/latest" >download the latest release</a>.
+
+Install the dependencies with `yarn install`
+
+## Usage
+
+- `yarn build` Compiles your JavaScript and CSS once for release.
+
+## Details
+
+#### `source/app.js`
+
+The SharkGate Cloudflare application code
+
+#### `source/app.css`
+
+The SharkGate Cloudflare application styles
+
+#### `install.json`
+
+Installer options for the SharkGate Cloudflare app (reference info can be found here <a href="https://www.cloudflare.com/apps/developer/docs/install-json">installer options</a>)
+
+#### `media/**`
+
+The SharkGate Cloudflare media directory
---
+++ source/app.js
@@ -0,0 +1,91 @@
+;(function () {
+  'use strict'
+
+  if (!window.addEventListener) return // Check for IE9+
+
+  const isPreview = INSTALL_ID === 'preview'
+  let options = INSTALL_OPTIONS
+  let product = INSTALL_PRODUCT
+  let element
+  const style = document.createElement('style')
+
+  function getMaxZIndex () {
+    var max = 0
+    var elements = document.getElementsByTagName('*')
+
+    Array.prototype.slice.call(elements).forEach(function (element) {
+      var zIndex = parseInt(document.defaultView.getComputedStyle(element).zIndex, 10)
+
+      max = zIndex ? Math.max(max, zIndex) : max
+    })
+
+    return max
+  }
+
+  function createSharkGateLogoElement (element) {
+    element = 'cloudflare-app'
+    element = document.createElement(element)
+    document.body.appendChild(element)
+    // element = INSTALL.createElement({
+    //   selector: 'body',
+    //   method: 'append'
+    // }, element)
+    element.id = 'sharkGateLogo'
+    return element
+  }
+
+  function removeSharkGateLogoElement () {
+    try {
+      // find a html element with id and remove if exists
+      var element = document.getElementById('createSharkGateLogoElement')
+      if (element) {
+        element.parentNode.removeChild(element)
+      }
+    } catch (e) {}
+  }
+
+  function removeAndAddElement () {
+    removeSharkGateLogoElement()
+    element = createSharkGateLogoElement(element)
+    element.setAttribute('app', 'website-protected-by-sharkgate')
+    element.setAttribute('data-position', options.position)
+    element.setAttribute('data-visibility', 'visible')
+    element.style.zIndex = getMaxZIndex() + 1
+
+    if (product) {
+      element.setAttribute('data-product-id', product.id)
+    }
+
+    element.innerHTML = `
+        <a class="sharkgate-icon" href="https://www.sharkgate.net" target="_blank">
+          <img src="./media/website-protection-via-sharkgate-logo.png" width="` + options.size + `%" height="` + options.size + `%" alt="website protection by sharkgate" />
+        </a>
+    `
+    element.appendChild(style)
+  }
+
+  function bootstrap () {
+    // alert('in bootstrap')
+    if (!isPreview && options.showStrategy === 'never') return
+    removeAndAddElement()
+  }
+
+  if (document.readyState === 'loading') {
+    // alert('Ready STate is loading')
+    document.addEventListener('DOMContentLoaded', bootstrap)
+  } else {
+    // alert('booty going for ')
+    bootstrap()
+  }
+
+  window.INSTALL_SCOPE = {
+    setOptions (nextOptions) {
+      options = nextOptions
+      removeAndAddElement()
+    },
+    setProduct (nextProduct) {
+      product = nextProduct
+      removeAndAddElement()
+    }
+  }
+}())
